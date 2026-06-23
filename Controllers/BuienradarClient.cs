36f8f3e956c49ad3db4570a27f1427f54e92cf27
rulianw <0909Wang@gmail.com>
2026-06-23 10:06:44 +0200

    versie 1

---
+++ Controllers/BuienradarClient.cs
@@ -0,0 +1,23 @@
+using System.Text.Json;
+using System.Net.Http;
+
+namespace WeatherApp
+{
+    public class BuienradarClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public BuienradarClient (HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<WeatherFeed> FetchAsync()
+        {
+            var url = "https://data.buienradar.nl/2.0/feed/json";
+            var json = await _httpClient.GetStringAsync(url);
+            var data = JsonSerializer.Deserialize<WeatherFeed>(json);
+            return data;
+        }
+    }
+}
